1efab295018735e699b5df23077f7b804ad5b941
XCtze <orbitalsscriptshowcase@gmail.com>
2025-05-01 13:38:19 -0500

    Update useless.c

--- useless.c
+++ useless.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
-
 int main() {
-    
     int x = 0;
     int y = x;
-    
     if (y == 0) {
         x = y;
     }
-    
     while (x == y) {
         break;
     }
-    
     return 0;
 }
